b88929d93bddde96e09790d577eaaafd99d6cd5c
João Sá Caetano <97513657+jscaetano@users.noreply.github.com>
2023-01-09 16:28:01 +0000

    more changes to cv page

--- index.js
+++ index.js
@@ -3,43 +3,51 @@ let animate = 0;
 
 
 function scrollFunction() {
+	let n = 0
+	let skillsHead = document.getElementById("skillsHead").innerHTML
+	if (skillsHead === "SKILLS")
+		n = 6
+	else
+		n = 3
 	let scrollheight = document.documentElement.scrollTop / 430
 	document.getElementById("container-header").style.background = "rgba(0,0,0," + scrollheight + ")"
 	document.getElementById("logobtnimage").style.filter = "invert(" + scrollheight + ")"
-	for(let i=0; i<6; i++){
+	for(let i=0; i<n; i++){
 		let id = i + "href"
 		document.getElementById(id).style.color = "rgba(" + scrollheight*200 + "," + scrollheight*200  + "," + scrollheight*200  + "," + "1)"
 	}
 	// console.log(scrollheight * 430)
 
-	if (scrollheight * 430 > 600) {
-		var aboutMe = document.getElementById("aboutMeid");
-		aboutMe.classList.add("anim-typewriter");
-	}
-
-	let widths = [0, '63', '35', '28', '28', '25', '73', '94', '60']
-
-	if (scrollheight * 430 > 300 && animate == 0) {
-		for(let i=1; i<9; i++)
-		{
-			document.getElementsByClassName('skill' + i)[0].animate([{
-				opacity: '0',
-				width: '0%',
-				padding: '0'
-			},
-			{
-				opacity: '100%',
-				width: widths[i] + '%',
-				padding: '0 16px 0 16px'
-			}
-			], {
-			duration: 1000,
-			});
-			document.getElementsByClassName('skill' + i)[0].style.opacity = '100%'
-			document.getElementsByClassName('skill' + i)[0].style.width = widths[i] + '%'
-			document.getElementsByClassName('skill' + i)[0].style.padding = '0 16px 0 16px'
+	if (n == '6') {
+		if (scrollheight * 430 > 600) {
+			var aboutMe = document.getElementById("aboutMeid");
+			aboutMe.classList.add("anim-typewriter");
 		}
-		animate = 1
+
+		let widths = [0, '63', '35', '28', '28', '25', '73', '94', '60']
+
+		if (scrollheight * 430 > 300 && animate == 0) {
+			for(let i=1; i<9; i++)
+			{
+				document.getElementsByClassName('skill' + i)[0].animate([{
+					opacity: '0',
+					width: '0%',
+					padding: '0'
+				},
+				{
+					opacity: '100%',
+					width: widths[i] + '%',
+					padding: '0 16px 0 16px'
+				}
+				], {
+				duration: 1000,
+				});
+				document.getElementsByClassName('skill' + i)[0].style.opacity = '100%'
+				document.getElementsByClassName('skill' + i)[0].style.width = widths[i] + '%'
+				document.getElementsByClassName('skill' + i)[0].style.padding = '0 16px 0 16px'
+			}
+			animate = 1
+		}
 	}
 }
 
